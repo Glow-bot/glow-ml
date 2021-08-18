b597a207bc93fabef1481f37d17939017df1e523
Googlefan256 <yyykakeru@gmail.com>
2021-08-18 20:54:17 +0900

    bug of cloudflare

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fetch = require("node-fetch")
 module.exports = async function(url){
- const res = await (await fetch(`https://glow.ml/api?url=${encodeURIComponent(url)}`)).text()
+ const res = await (await fetch(`https://glowml.googlefan.repl.co/api?url=${encodeURIComponent(url)}`)).text()
  if(res.includes("is not"))return "not-an-url"
+ if(res.length !== 6)return "error"
  return res;
 }
