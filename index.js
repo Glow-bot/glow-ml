2080c8bf791af7aaa09a2d44da422debee4f9d1d
すずねーう <suzuneu@suzuneu.com>
2021-11-18 12:52:42 +0900

    change: types用の変更「

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const fetch = require("node-fetch")
-module.exports = async function(url){
+const fetch = require("node-fetch");
+
+async function shorten(url){
  const res = await (await fetch(`https://glowml.googlefan.repl.co/api?url=${encodeURIComponent(url)}`)).text()
  if(res.includes("is not"))return "not-an-url"
  if(res.length !== 6)return "error"
  return res;
 }
+
+module.exports = shorten;
